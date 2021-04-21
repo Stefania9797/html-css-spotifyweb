019f9710844d52d00613f6bd89aa867cd5baf754
Stefania9797 <stefaniamastrel@gmail.com>
2021-04-21 14:00:39 +0200

    modify "recently played" names

---
+++ assets/main.js
@@ -0,0 +1,11 @@
+//A quiet place playlist
+document.getElementById("horror").innerHTML="Horror Movie"
+document.getElementById("horror_subt").innerHTML="Playlist con soundtrack horror"
+
+//A cure for wellness playlist
+document.getElementById("cure").innerHTML="Relax"
+document.getElementById("cure_subt").innerHTML="Playlist con brani rilassanti"
+
+//Stranger things playlist
+document.getElementById("stranger").innerHTML="Stranger things soundtrack"
+document.getElementById("stranger_subt").innerHTML="Soundtrack dalla serie Netflix stranger things"
